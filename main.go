16e3a3437cecbb7a9de34e7186907b3492ea8860
theMyle <99082612+theMyle@users.noreply.github.com>
2026-03-16 12:24:14 +0800

    middleware and learned about receiver methods

--- main.go
+++ main.go
@@ -1,20 +1,73 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
 
 type Server struct {
 	Addr string
 }
 
-func main()  {
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// show metrics (hit count)
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileServerHits.Load())))
+}
+
+// set metrics back to zero
+func (cfg *apiConfig) handleMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("Metrics reset to 0"))
+}
+
+func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// file server
+	mux.Handle("/app/",
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix("/app/", http.FileServer(http.Dir("."))),
+		),
+	)
 
-	server := &http.Server{
+	// health check
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("OK"))
+	})
+
+	// metrics
+	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handleMetricsReset)
+
+	srv := &http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 
-	server.ListenAndServe()
+	log.Printf("Running server at port %s (http://localhost%s)\n\n", srv.Addr, srv.Addr)
+
+	fmt.Println("Routes:")
+	fmt.Println("\t/app:")
+	fmt.Println("\t/healthz:")
+
+	srv.ListenAndServe()
 }
